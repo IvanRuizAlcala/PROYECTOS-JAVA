57e63295a54262016a7439f0b77a62c20a9e0d2f
IvanRuizAlcala <156293263+IvanRuizAlcala@users.noreply.github.com>
2024-05-10 10:33:19 +0200

    EJERCICIO 12
    nuevo ejercicio 12

--- ejercicio_12/src/App/App.java
+++ ejercicio_12/src/App/App.java
@@ -1,14 +1,49 @@
 package App;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import Class.GestionViajes;
+import Class.OpenBBDD;
 import Class.Viaje;
 
 public class App {
 
-	public static void main(String[] args) {
-		Viaje viaje1;
-		Viaje viaje2;
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		//Datos
+		OpenBBDD abrirViajesPaisUno = new OpenBBDD("paises1.txt");
+		OpenBBDD abrirViajesPaisDos = new OpenBBDD("paises2.txt");
+		GestionViajes gestionarViajesUno;
+		GestionViajes gestionarViajesDos;
+		//Programa
+		System.out.println(" bienvenidos a Sr.Wile{Systems}");
+		System.out.println(" vamos a Cargar los archivos");
+		System.out.print("0% ");
+		for (int i = 0; i < 10; i++) {
+			System.out.print("---");
+		}
+		abrirViajesPaisUno.abrirArchivo();
+		ArrayList<String> archivoCompletoPaisUno = abrirViajesPaisUno.insertarDatos();
+		abrirViajesPaisUno.cerrarArchivo();
+		abrirViajesPaisDos.abrirArchivo();
+		ArrayList<String> archivoCompletoPaisDos = abrirViajesPaisDos.insertarDatos();
+		abrirViajesPaisDos.cerrarArchivo();
 		
-
+		System.out.println(" 100% ");
+		System.out.println(" archivos cargados");
+		System.out.println(" vamos a gestionar los viajes 1");
+		gestionarViajesUno = new GestionViajes(archivoCompletoPaisUno);
+		ArrayList<Viaje> cargarViajesUno = gestionarViajesUno.cargarViajes();
+		for (Viaje viaje : cargarViajesUno) {
+			System.out.println(viaje);
+		}
+		System.out.println(" vamos a gestionar los viajes 2");
+		gestionarViajesDos = new GestionViajes(archivoCompletoPaisDos);
+		ArrayList<Viaje> cargarViajesDos = gestionarViajesDos.cargarViajes();
+		for (Viaje viaje : cargarViajesDos) {
+			System.out.println(viaje);
+		}
 	}
 
 }
--- ejercicio_12/src/Class/GestionViajes.java
+++ ejercicio_12/src/Class/GestionViajes.java
@@ -3,15 +3,24 @@ package Class;
 import java.util.ArrayList;
 
 public class GestionViajes {
-ArrayList<Viaje> misViajes = new ArrayList<>();
+ArrayList<String> misViajes = new ArrayList<>();
 
-public GestionViajes(ArrayList<Viaje> misViajes) {
+public GestionViajes(ArrayList<String> misViajes) {
 	super();
 	this.misViajes = misViajes;
 }
-public ArrayList<Viaje> cargarViajes(ArrayList<Viaje> misViajes){
-	
-	return misViajes;
+public ArrayList<Viaje> cargarViajes(){
+	ArrayList<Viaje> retornarViajes = new ArrayList<>();
+	 String nombrePais = " ";
+	 int id = 0;
+	 Viaje viaje;
+	 	for (int i = 0; i < misViajes.size(); i++) {
+			nombrePais = misViajes.get(i);
+			id++;
+			viaje = new Viaje(id,nombrePais,nombrePais.length() );
+			retornarViajes.add(viaje);
+		}
+	return retornarViajes;
 	
 }
 }
